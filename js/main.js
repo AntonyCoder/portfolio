62cbbd5e0f669f39194d44534d4d6c4d95bc0bc4
AntonyCoder <69346513+AntonyCoder@users.noreply.github.com>
2020-11-17 20:05:01 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,49 @@
-AOS.init();
-var mySwiper = new Swiper('.swiper-container', {
+//слайдер
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  effect: 'fade',
   pagination: {
-    el: '.projects-pagination',
-    bulletClass: 'projects-bullet',
-    bulletActiveClass: 'projects-bullet-active',
-    clickable: true
+    el: '.swiper-pagination',
+    clickable: true,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+//карта
+var map;
+var marker;
+function initMap() {
+  map = new ymaps.Map("yandexmap", {
+    center: [47.255866, 38.893980],
+    zoom: 18
+  });
+  marker = new ymaps.Placemark([47.255866, 38.893980], {
+  });
+  map.geoObjects.add(marker);
+}
+ymaps.ready(initMap);
+
+//плавная прокрутка
+$(function () {
+  $("a[href^='#']").click(function () {
+    var _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+  });
+});
+
+// зафиксированное меню
+jQuery(function ($) {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 160) {
+      $('#navigation').addClass('fixed');
+    }
+    else if ($(this).scrollTop() < 160) {
+      $('#navigation').removeClass('fixed');
+    }
+  });
 });
